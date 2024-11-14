a253634bdafaba1785ac1a98d52082036ee744a2
Joel @ Dell3260 <sindicatoesp@gmail.com>
2024-11-14 10:43:45 +0000

    WIP: sso Azure b2c login for regular user v3.

--- app/routes/user.accept-invite/route.tsx
+++ app/routes/user.accept-invite/route.tsx
@@ -14,49 +14,129 @@ import {
 	Field,
 	SubmitButton,
 	FieldErrors,
-} from "~/components/form"
+} from "~/components/form";
 import { formStringData } from "~/util/httputil";
 import {
 	createUserSession
 } from "~/util/session";
+import { 
+	configSsoAzureB2C 
+} from "~/util/config";
+import { 
+	SSOAzureB2C as interfaceSSOAzureB2C, 
+	baseURL,
+	decodeToken,
+	loginGetCode
+} from "~/util/ssoauzeb2c";
 import {
 	acceptInvite,
 	AcceptInviteFieldsFromMap,
 	validateInviteCode,
 } from "~/backend.server/models/user"
-import { loginGetCode } from "~/util/ssoauzeb2c"
+import {
+	setupAccountSSOAzureB2C,
+	setupAdminAccountFieldsFromMap,
+	loginAzureB2C,
+	registerAzureB2C,
+} from "~/backend.server/models/user";
 
 export const loader = async ({request}:LoaderFunctionArgs) => {
+	const jsonAzureB2C:interfaceSSOAzureB2C = configSsoAzureB2C();
+	const urlSSOCode2Token = `${ baseURL() }/token?p=${ jsonAzureB2C.login_userflow }`;
 	const url = new URL(request.url);
 	const inviteCode = url.searchParams.get("inviteCode") || "";
 	const sso = url.searchParams.get("sso") || "";
 	const action = url.searchParams.get("action") || "";
 	const state = url.searchParams.get("state") || "";
-	const code = url.searchParams.get("code") || "";
+	const queryStringCode = url.searchParams.get("code") || "";
 	const res = await validateInviteCode(inviteCode);
+
+	let data: { [key: string]: string } = { email:'', password:'', firstName:'', lastName:'' };
+	let token:object = {};
+	let token_idp:object = {};
 
 	if ( action == 'sso_azure_b2c' ) {
 		return loginGetCode(inviteCode);
 	}
 
-	if ( state ) {
+	if ( state && queryStringCode ) {
 		console.log( state );
 		console.log( decodeURI(state) );
 		console.log( decodeURIComponent(state) );
 		console.log( encodeURIComponent(state) );
 		console.log( '' );
+
+		try {
+			// WORKING
+			const response = await fetch(urlSSOCode2Token, {
+				method: 'POST',
+				headers:{
+				'Content-Type': 'application/x-www-form-urlencoded'
+				},    
+				body: new URLSearchParams({
+					'client_id': jsonAzureB2C.client_id,
+					'client_secret': jsonAzureB2C.client_secret,
+					'code': queryStringCode,
+					'grant_type': 'authorization_code',
+				})
+			});
+
+			const result = await response.json();
+			console.log( result );
+			if ("id_token" in result) {
+				token=decodeToken( result.id_token );
+				console.log(token);
+				if ("idp_access_token" in token) {
+					console.log( token.idp_access_token );
+					token_idp=decodeToken( String(token.idp_access_token) );
+					console.log( token_idp );
+					if ('family_name' in token_idp) {
+						data['lastName'] = String(token_idp.family_name);
+					}
+					if ('given_name' in token_idp) {
+						data['firstName'] = String(token_idp.given_name);
+					}
+					if ('unique_name' in token_idp) {
+						data['email'] = String(token_idp.unique_name);
+					}
+				}
+				else {
+					if ('family_name' in token) {
+						data['lastName'] = String(token.family_name);
+					}
+					if ('given_name' in token) {
+						data['firstName'] = String(token.given_name);
+					}
+					if ('emails' in token) {
+						data['email'] = String(token.emails);
+					}
+				}
+			}
+			else if ('error' in result && 'error_description' in result) {
+				return json({ errors: result.error_description, inviteCode: '', inviteCodeValidation: { ok: false, error: '' } }, { status: 500 });
+			}
+
+			let retLogin = await registerAzureB2C(data['email'], data['firstName'], data['lastName']);
+			if (!retLogin.ok) {
+				return json({ errors:retLogin.error, inviteCode: '', inviteCodeValidation: { ok: false, error: '' } });
+			}
+
+			const headers = await createUserSession(retLogin.userId);
+			return redirect("/", { headers });
+		}
+		catch (error) { 
+			console.error('Error:', error); 
+			return json({ errors:error, inviteCode: '', inviteCodeValidation: { ok: false, error: '' } });
+		}
+	
 	}
 
-	if ( code ) {
-		console.log( code );
-		console.log( '' );
-	}
 
 	return json({
 		inviteCode: inviteCode,
 		sso: sso,
 		inviteCodeValidation: res,
-		code: code,
+		code: queryStringCode,
 		state: state,
 	});
 };
@@ -75,7 +155,7 @@ export const action = async ({ request }: ActionFunctionArgs) => {
 
 export default function Screen() {
 	const loaderData = useLoaderData<typeof loader>();
-	const inviteCode = loaderData.inviteCode
+	const inviteCode = loaderData.inviteCode;
 	const actionData = useActionData<typeof action>();
 
 	const errors = actionData?.errors
