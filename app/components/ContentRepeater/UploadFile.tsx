7f0412b62511a51079b6b61de5ecc1785472f895
Ryan Sandigan <ryanzkizen@yahoo.com>
2025-01-27 13:50:58 +0800

    Changed default public path

--- app/components/ContentRepeater/UploadFile.tsx
+++ app/components/ContentRepeater/UploadFile.tsx
@@ -12,7 +12,7 @@ interface Item {
 const debug = false;
 
 class ContentRepeaterUploadFile {
-  static delete(itemsData: string, publicPath: string = process.env.PUBLIC_PATH || "/app/public"): string {
+  static delete(itemsData: string, publicPath: string = process.env.PUBLIC_PATH || "/data/public"): string {
     let items: Item[];
 
     try {
@@ -83,7 +83,7 @@ class ContentRepeaterUploadFile {
     itemsData: string,
     tempPath: string,
     destinationPath: string,
-    publicPath: string = process.env.PUBLIC_PATH || "/app/public"
+    publicPath: string = process.env.PUBLIC_PATH || "/data/public"
   ): string {
     let items: Item[];
   
