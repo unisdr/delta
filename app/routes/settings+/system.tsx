49119ae6c5b763f30a9b4f25066c08e70aea4f24
Joel @ WSL CHUWI <sindicatoesp@gmail.com>
2025-02-02 20:13:04 +0700

    Provision for application curreny and application version appearing in Admin Settings.

--- app/routes/settings+/system.tsx
+++ app/routes/settings+/system.tsx
@@ -7,6 +7,7 @@ import { authLoaderGetAuth, authLoaderWithPerm } from "~/util/auth";
 import { getSupportedTimeZone } from "~/util/timezone";
 
 import { getCurrency } from "~/util/currency";
+import { configCurrencies, configApplicationVersion } from  "~/util/config";
 
 import { NavSettings } from "~/routes/settings/nav";
 import { MainContainer } from "~/frontend/container";
@@ -15,13 +16,21 @@ import { Input } from "~/frontend/form";
 export const loader = authLoaderWithPerm("ViewData", async (loaderArgs) => {
   const { user } = authLoaderGetAuth(loaderArgs);
   const timeZones: string[] = getSupportedTimeZone();
-  const currency: string[] = getCurrency();
+  const currency: string[] = configCurrencies();
   const systemLanguage: string[] = ["English"];
+
+
+  const confAppVersion = await configApplicationVersion().then(version => {
+    return version;
+  }).catch(error => {
+    console.error('Error:', error);
+  });
 
   return {
     message: `Hello ${user.email}`,
-    currency: currency,
-    timeZones: timeZones,
+    currencyArray: currency,
+    timeZonesArray: timeZones,
+    appVersion: confAppVersion,
     systemLanguage: systemLanguage,
   };
 });
@@ -46,7 +55,7 @@ export default function Settings() {
     <MainContainer title="System settings" headerExtra={<NavSettings />}>
       <>
         <div className="flex">
-          <label style={{ margin: "0px 20px" }}>
+          {/* <label style={{ margin: "0px 20px" }}>
             <strong>Time zone</strong> &nbsp;
             <select
               style={{
@@ -59,14 +68,14 @@ export default function Settings() {
               <option disabled value="">
                 Select from list
               </option>
-              {loaderData.timeZones.map((item, index) => (
+              {loaderData.timeZonesArray.map((item, index) => (
                 <option key={index} value={item}>
                   {item}
                 </option>
               ))}
             </select>
-          </label>
-          <label>
+          </label> */}
+          <label style={{ margin: "0px 20px" }}>
             <strong>System language</strong> &nbsp;
             <select
               style={{
@@ -86,7 +95,7 @@ export default function Settings() {
               ))}
             </select>
           </label>
-          {/* <label>
+          <label>
             <strong>Currency</strong> &nbsp;
             <select
               style={{
@@ -99,23 +108,20 @@ export default function Settings() {
               <option disabled value="">
                 Select from list
               </option>
-              {loaderData.currency.map((item, index) => (
+              {loaderData.currencyArray.map((item, index) => (
                 <option key={index} value={item}>
                   {item}
                 </option>
               ))}
             </select>
-          </label> */}
+          </label>
         </div>
         <div className="flex">
           <ul>
             <li>
-              <strong>Date installed:</strong> 09-01-2025
+              <strong>Current version of the application:</strong> { loaderData.appVersion }
             </li>
-            <li>
-              <strong>Last updated:</strong> 12-01-2025 T00:00:0000:00
-            </li>
-            <li>
+            {/* <li>
               <strong>Update available:</strong> 00.00.02:{" "}
               <Link to={""}>
                 <u>See release notes</u>{" "}
@@ -125,7 +131,7 @@ export default function Settings() {
                   style={{ marginRight: "8px" }}
                 />
               </Link>
-            </li>
+            </li> */}
             <li>
               <strong>System up to date</strong>
             </li>
