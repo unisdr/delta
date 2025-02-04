4eaef616aaab226d57702c898bdd19af79729a22
Haroon Nasher <haroon_foad2003@yahoo.com>
2025-02-04 14:58:45 +0700

    Revert "#32 Enable log auditing when update, delete, insert on disaster record /disaster-record"
    This reverts commit 00678877e00d0fc59bb01fe386127c09939115fe.

--- app/routes/disaster-record+/$id.tsx
+++ app/routes/disaster-record+/$id.tsx
@@ -3,54 +3,20 @@ import {
 } from "~/frontend/disaster-record/form";
 
 import {
-	createViewLoaderPublicApprovedWithAuditLog,
+	createViewLoaderPublicApproved,
 } from "~/backend.server/handlers/form";
 
 import {
 	ViewScreenPublicApproved
 } from "~/frontend/form";
 import {disasterRecordsById} from "~/backend.server/models/disaster_record";
-import { disasterRecordsTable } from "~/drizzle/schema";
-import { getTableName } from "drizzle-orm";
-import { LoaderFunctionArgs } from "@remix-run/node";
-import { useLoaderData } from "@remix-run/react";
-import AuditLogHistory from "~/components/AuditLogHistory";
 
-interface LoaderData{
-	item: any;
-	isPublic: boolean;
-	auditLogs: any[];
-}
-
-
-export const loader = async ({
-	request,
-	params,
-	context}: LoaderFunctionArgs): Promise<LoaderData> => {
-	const { id } = params;
-	if(!id) {
-		throw new Response("ID is required", { status: 400 });
-	}
-
-	const loaderFunction = createViewLoaderPublicApprovedWithAuditLog({
-		getById: disasterRecordsById,
-		recordId: id,
-		tableName: getTableName(disasterRecordsTable),
-	});
-
-	const result = await loaderFunction({request, params, context});
-	return {...result, auditLogs: result.auditLogs ?? []};
-};
-
+export const loader = createViewLoaderPublicApproved({
+	getById: disasterRecordsById
+});
 
 export default function Screen() {
-	const { auditLogs } = useLoaderData<typeof loader>();
-	return (
-		<>
-			<ViewScreenPublicApproved viewComponent={DisasterRecordsView} />
-			<br/>
-			<h3>Audit Log History</h3>
-			<AuditLogHistory auditLogs={auditLogs} />
-		</>
-	);
+	return ViewScreenPublicApproved({
+		viewComponent: DisasterRecordsView
+	})
 }
