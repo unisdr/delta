577e789c19552c94a2c227140af2c613bf90e6a7
DDEV User <nobody@example.com>
2025-03-21 17:58:42 +0700

    WIP - disaster events analtics - sector effects.

--- app/routes/analytics+/disaster-events.tsx
+++ app/routes/analytics+/disaster-events.tsx
@@ -676,6 +676,113 @@ function DisasterEventsAnalysisContent() {
             </section>
           </>
         )}
+
+          <section className="dts-page-section">
+              <div className="mg-container">
+                <h2 className="dts-heading-2">{ ld.cpDisplayName } impacts on sectors</h2>
+
+                  <div className="mg-grid mg-grid__col-3">
+                    <div className="dts-data-box">
+                      <div className="dts-indicator dts-indicator--target-box-b">
+                        <span>Damage pie chart</span>
+                      </div>
+                    </div>
+                    <div className="dts-data-box">
+                      <div className="dts-indicator dts-indicator--target-box-c">
+                        <span>Losses pie chart</span>
+                      </div>
+                    </div>
+
+                    <div className="dts-data-box">
+                      <div className="dts-indicator dts-indicator--target-box-d">
+                        <span>Recovery pie chart</span>
+                      </div>
+                    </div>
+                  </div>
+
+                  <div className="mg-grid mg-grid__col-1">
+                    <div className="dts-data-box">
+                      <div className="dts-indicator dts-indicator--target-box-a">
+                        <span>Bar chart</span>
+                      </div>
+                    </div>
+                  </div>
+              </div>
+          </section>
+
+
+          <section className="dts-page-section">
+              <div className="mg-container">
+                <h2 className="dts-heading-3">Details of effects</h2>
+
+                <div className="mg-grid mg-grid__col-3">
+                  <div className="dts-data-box">
+                    <h3 className="dts-body-label">
+                      <span>Damage in [sector] in [currency]</span>
+                    </h3>
+                    <div className="dts-indicator dts-indicator--target-box-b">
+                      <span> todo </span>
+                    </div>
+                  </div>
+                  <div className="dts-data-box">
+                    <h3 className="dts-body-label">
+                      <span>Losses in [sector] in [currency]</span>
+                    </h3>
+                    <div className="dts-indicator dts-indicator--target-box-c">
+                      <span> todo </span>
+                    </div>
+                  </div>
+
+                  <div className="dts-data-box">
+                    <h3 className="dts-body-label">
+                      <span>Recovery in [sector] in [currency]</span>
+                    </h3>
+                    <div className="dts-indicator dts-indicator--target-box-d">
+                      <span> todo </span>
+                    </div>
+                  </div>
+
+
+                </div>
+
+                <div className="mg-grid mg-grid__col-3">
+                    <div className="dts-data-box">
+                      <div className="dts-indicator dts-indicator--target-box-b">
+                        <span>Sub-sector pie chart</span>
+                      </div>
+                    </div>
+                    <div className="dts-data-box">
+                      <div className="dts-indicator dts-indicator--target-box-c">
+                        <span>Sub-sector pie chart</span>
+                      </div>
+                    </div>
+
+                    <div className="dts-data-box">
+                      <div className="dts-indicator dts-indicator--target-box-d">
+                        <span>Sub-sector pie chart</span>
+                      </div>
+                    </div>
+                  </div>
+
+              </div>
+          </section>
+
+
+          <section className="dts-page-section">
+              <div className="mg-container">
+                <h2 className="dts-heading-3">Detailed effects in [selected sector]</h2>
+
+                <div className="mg-grid mg-grid__col-1">
+                  <div className="dts-data-box">
+                    <div className="dts-indicator dts-indicator--target-box-a">
+                      <span>Table raw data</span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+          </section>
+
+          
       
 
       </>)}
