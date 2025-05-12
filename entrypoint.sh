f5d8b557cf4f60f1563a8b1d4497427003a82316
Junior Dieka <jnrdieka@gmail.com>
2025-05-12 15:58:39 +0700

    chore: Remove unused SSH setup script

--- entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Ensure the SSH directory exists and has the right permissions
-mkdir -p /root/.ssh
-chmod 700 /root/.ssh
-chmod 600 /root/.ssh/*
-
-# Execute the command passed to the docker run command
-exec "$@"
