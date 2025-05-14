82abb17f93a97cbe136d74688ae8a2e1c5c0e87f
Haroon Nasher <haroon_foad2003@yahoo.com>
2025-05-14 08:10:32 +0700

    #165 fixing missing user session when clicking on disaster record id while not logged in

--- app/routes/disaster-record+/$id.tsx
+++ app/routes/disaster-record+/$id.tsx
@@ -1,60 +1,68 @@
-import {
-	DisasterRecordsView,
-} from "~/frontend/disaster-record/form";
+import { DisasterRecordsView } from "~/frontend/disaster-record/form";
 
 import {
+	createViewLoaderPublicApproved,
 	createViewLoaderPublicApprovedWithAuditLog,
 } from "~/backend.server/handlers/form/form";
 
-import {
-	ViewScreenPublicApproved
-} from "~/frontend/form";
-import {disasterRecordsById} from "~/backend.server/models/disaster_record";
+import { ViewScreenPublicApproved } from "~/frontend/form";
+import { disasterRecordsById } from "~/backend.server/models/disaster_record";
 import {
 	disasterRecordsTable,
 	disruptionTable,
 	lossesTable,
 	damagesTable,
-  } from "~/drizzle/schema";
+} from "~/drizzle/schema";
 import { getTableName } from "drizzle-orm";
 import { LoaderFunctionArgs } from "@remix-run/node";
 
 import { dr } from "~/db.server";
 import { contentPickerConfig } from "./content-picker-config";
 import { sql, eq } from "drizzle-orm";
+import { optionalUser } from "~/util/auth";
 
-interface LoaderData{
+interface LoaderData {
 	item: any;
 	isPublic: boolean;
-	auditLogs: any[];
+	auditLogs?: any[];
 }
-
 
 export const loader = async ({
 	request,
 	params,
-	context}: LoaderFunctionArgs): Promise<LoaderData> => {
+	context,
+}: LoaderFunctionArgs): Promise<LoaderData> => {
 	const { id } = params;
-	if(!id) {
+	if (!id) {
 		throw new Response("ID is required", { status: 400 });
 	}
 
-	const loaderFunction = createViewLoaderPublicApprovedWithAuditLog({
-		getById: disasterRecordsById,
-		recordId: id,
-		tableName: getTableName(disasterRecordsTable),
-	});
+	// Check if user is logged in
+	const session = await optionalUser(request);
+	const loaderFunction = session
+		? createViewLoaderPublicApprovedWithAuditLog({
+				getById: disasterRecordsById,
+				recordId: id,
+				tableName: getTableName(disasterRecordsTable),
+		  })
+		: createViewLoaderPublicApproved({
+				getById: disasterRecordsById,
+		  });
 
-	const result = await loaderFunction({request, params, context}); 
+	const result = await loaderFunction({ request, params, context });
 
-	const cpDisplayName = await contentPickerConfig.selectedDisplay(dr, result.item.disasterEventId) ?? '';
+	const cpDisplayName =
+		(await contentPickerConfig.selectedDisplay(
+			dr,
+			result.item.disasterEventId
+		)) ?? "";
 
 	const disasterId = id;
 	const disasterRecord = await dr
-    .select({
-      disaster_id: disasterRecordsTable.id,
-      disaster_spatial_footprint: disasterRecordsTable.spatialFootprint,
-      disruptions: sql`
+		.select({
+			disaster_id: disasterRecordsTable.id,
+			disaster_spatial_footprint: disasterRecordsTable.spatialFootprint,
+			disruptions: sql`
         COALESCE(
           jsonb_agg(
             jsonb_build_object(
@@ -64,7 +72,7 @@ export const loader = async ({
           ) FILTER (WHERE ${disruptionTable.id} IS NOT NULL), '[]'::jsonb
         )
       `.as("disruptions"),
-      losses: sql`
+			losses: sql`
         COALESCE(
           jsonb_agg(
             jsonb_build_object(
@@ -74,7 +82,7 @@ export const loader = async ({
           ) FILTER (WHERE ${lossesTable.id} IS NOT NULL), '[]'::jsonb
         )
       `.as("losses"),
-      damages: sql`
+			damages: sql`
         COALESCE(
           jsonb_agg(
             jsonb_build_object(
@@ -84,19 +92,21 @@ export const loader = async ({
           ) FILTER (WHERE ${damagesTable.id} IS NOT NULL), '[]'::jsonb
         )
       `.as("damages"),
-    })
-    .from(disasterRecordsTable)
-    .leftJoin(disruptionTable, eq(disasterRecordsTable.id, disruptionTable.recordId))
-    .leftJoin(lossesTable, eq(disasterRecordsTable.id, lossesTable.recordId))
-    .leftJoin(damagesTable, eq(disasterRecordsTable.id, damagesTable.recordId))
-    .where(eq(disasterRecordsTable.id, disasterId))
-    .groupBy(disasterRecordsTable.id, disasterRecordsTable.spatialFootprint);
+		})
+		.from(disasterRecordsTable)
+		.leftJoin(
+			disruptionTable,
+			eq(disasterRecordsTable.id, disruptionTable.recordId)
+		)
+		.leftJoin(lossesTable, eq(disasterRecordsTable.id, lossesTable.recordId))
+		.leftJoin(damagesTable, eq(disasterRecordsTable.id, damagesTable.recordId))
+		.where(eq(disasterRecordsTable.id, disasterId))
+		.groupBy(disasterRecordsTable.id, disasterRecordsTable.spatialFootprint);
 
 	const extendedItem = { ...result.item, cpDisplayName, disasterRecord };
 	
 	return {...result, item: extendedItem};
 };
-
 
 export default function Screen() {
 	return (
