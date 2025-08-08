c1421b95b662d7acb12dbcbd19d6095bf7194d5f
JuniorDieka <jnrdieka@gmail.com>
2025-08-08 15:27:30 +0700

    #222 fix(damages): enforce settings check and streamline view loader creation

--- app/routes/disaster-record+/edit-sub.$disRecId+/damages+/$id.tsx
+++ app/routes/disaster-record+/edit-sub.$disRecId+/damages+/$id.tsx
@@ -1,29 +1,30 @@
-import { damagesById } from "~/backend.server/models/damages";
+import { damagesById, fieldsDefView } from "~/backend.server/models/damages";
 
 import { DamagesView } from "~/frontend/damages";
 
-import { fieldsDefView } from "~/backend.server/models/damages";
-import { createViewLoader } from "~/backend.server/handlers/form/form";
 import { ViewScreenWithDef } from "~/frontend/form";
 import { LoaderFunctionArgs } from "@remix-run/server-runtime";
 import { getCountrySettingsFromSession } from "~/util/session";
 
 export const loader = async (loaderArgs: LoaderFunctionArgs) => {
 	const { request } = loaderArgs;
-	const countryAccountsId = await getCountrySettingsFromSession(request);
-	if (!countryAccountsId) {
-		throw new Response("Unauthorized", { status: 401 });
-	}
 	const settings = await getCountrySettingsFromSession(request);
+
+	if (!settings) {
+		throw new Response("Unauthorized access", { status: 401 });
+	}
 
 	const currencies = settings.currencyCode ? [settings.currencyCode] : ["USD"];
 
-	return createViewLoader({
-		getById: damagesById,
-		extra: async (_item) => {
-			return { def: await fieldsDefView(currencies) };
-		},
-	})(loaderArgs);
+	return (await (async () => {
+		const result = (await (await import("~/backend.server/handlers/form/form")).createViewLoader)({
+			getById: damagesById,
+			extra: async (_item) => {
+				return { def: await fieldsDefView(currencies) };
+			},
+		})(loaderArgs);
+		return result;
+	})());
 };
 
 export default function Screen() {
