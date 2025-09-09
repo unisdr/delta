586677d8ac829aef0f8829f38dbcf18b5c8170b7
Haroon Nasher <haroon_foad2003@yahoo.com>
2025-09-09 13:51:34 +0700

    #284 change id of dts_system_info to match the singleton function dts_system_info_singleton

--- app/drizzle/migrations/20250908093239_init_dts_system_info.sql
+++ app/drizzle/migrations/20250908093239_init_dts_system_info.sql
@@ -4,4 +4,4 @@ DELETE FROM public.dts_system_info;
 INSERT INTO
 	public.dts_system_info (id, db_version_no, app_version_no)
 VALUES
-	('54809ea1-6d7d-443b-b051-0d7496c91de1', '1.0.0', '0.0.9');
+	('73f0defb-4eba-4398-84b3-5e6737fec2b7', '1.0.0', '0.0.9');
