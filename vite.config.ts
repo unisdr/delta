1df9c2c92a506e7d12b721cf210d9e186bbc4e38
Haroon Nasher <haroon_foad2003@yahoo.com>
2025-10-08 08:22:12 +0700

    #320 fix issue of directory import with node

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
-import {vitePlugin as remix} from "@remix-run/dev";
-import {defineConfig} from "vite";
+import { vitePlugin as remix } from "@remix-run/dev";
+import { defineConfig } from "vite";
 import path from "path";
-import {flatRoutes} from "remix-flat-routes";
+import { flatRoutes } from "remix-flat-routes";
 
 declare module "@remix-run/server-runtime" {
 	interface Future {
@@ -10,37 +10,49 @@ declare module "@remix-run/server-runtime" {
 }
 
 export default defineConfig({
+	ssr: {
+		noExternal: ["primereact", "primeflex", "primeicons"],
+	},
 	plugins: [
 		remix({
-			routes: async defineRoutes => {
+			routes: async (defineRoutes) => {
 				// Integrate flatRoutes to dynamically define Remix routes
 				return flatRoutes("routes", defineRoutes);
 			},
 			future: {
 				v3_singleFetch: true,
 				v3_fetcherPersist: true,
-    			v3_lazyRouteDiscovery: true,
-    			v3_relativeSplatPath: true,
-    			v3_throwAbortReason: true,
+				v3_lazyRouteDiscovery: true,
+				v3_relativeSplatPath: true,
+				v3_throwAbortReason: true,
 			},
 		}),
 		{
-			name: 'custom-security-headers',
+			name: "custom-security-headers",
 			configureServer(server) {
 				server.middlewares.use((req, res, next) => {
 					req.url;
 					res.setHeader("Referrer-Policy", "no-referrer-when-downgrade");
-					res.setHeader("Permissions-Policy", "geolocation=(self), microphone=(), camera=(), fullscreen=(self), payment=()");
+					res.setHeader(
+						"Permissions-Policy",
+						"geolocation=(self), microphone=(), camera=(), fullscreen=(self), payment=()"
+					);
 					res.setHeader("X-Frame-Options", "SAMEORIGIN");
 					res.setHeader("X-Powered-By", "");
 					res.setHeader("X-XSS-Protection", "1; mode=block");
 					res.setHeader("X-Content-Type-Options", "nosniff");
-					res.setHeader("Cache-Control", "no-store, no-cache, must-revalidate, proxy-revalidate");
-					res.setHeader("Content-Security-Policy", "default-src 'self'; script-src 'self' 'unsafe-inline' blob: https://unpkg.com https://cdnjs.cloudflare.com https://*.preventionweb.net https://ajax.cloudflare.com https://static.cloudflareinsights.com; style-src 'self' 'unsafe-inline' https://unpkg.com https://cdnjs.cloudflare.com https://*.preventionweb.net; img-src 'self' data: blob: https://*.preventionweb.net https://*.basemaps.cartocdn.com https://*.tile.openstreetmap.org https://maps.google.com https://rawgit.com; font-src 'self' https:; connect-src 'self' https: wss: ws: https://data.undrr.org https://nominatim.openstreetmap.org https://unb2c.b2clogin.com https://*.b2clogin.com; worker-src 'self' blob:; frame-src 'self' https://unb2c.b2clogin.com https://*.b2clogin.com; form-action 'self' https://*.b2clogin.com; object-src 'self' data: blob:; base-uri 'self'; frame-ancestors 'self';");
+					res.setHeader(
+						"Cache-Control",
+						"no-store, no-cache, must-revalidate, proxy-revalidate"
+					);
+					res.setHeader(
+						"Content-Security-Policy",
+						"default-src 'self'; script-src 'self' 'unsafe-inline' blob: https://unpkg.com https://cdnjs.cloudflare.com https://*.preventionweb.net https://ajax.cloudflare.com https://static.cloudflareinsights.com; style-src 'self' 'unsafe-inline' https://unpkg.com https://cdnjs.cloudflare.com https://*.preventionweb.net; img-src 'self' data: blob: https://*.preventionweb.net https://*.basemaps.cartocdn.com https://*.tile.openstreetmap.org https://maps.google.com https://rawgit.com; font-src 'self' https:; connect-src 'self' https: wss: ws: https://data.undrr.org https://nominatim.openstreetmap.org https://unb2c.b2clogin.com https://*.b2clogin.com; worker-src 'self' blob:; frame-src 'self' https://unb2c.b2clogin.com https://*.b2clogin.com; form-action 'self' https://*.b2clogin.com; object-src 'self' data: blob:; base-uri 'self'; frame-ancestors 'self';"
+					);
 					next();
 				});
-			}
-		}
+			},
+		},
 	],
 	resolve: {
 		alias: {
