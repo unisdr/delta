085c5efa9c667091710de2f7f85092da4c2e0667
Joel @ Dell3260 <sindicatoesp@gmail.com>
2025-10-09 13:58:21 +0700

    Exposed instance type in systems settings page.

--- app/routes/settings+/system.tsx
+++ app/routes/settings+/system.tsx
@@ -14,7 +14,7 @@ import { getSystemInfo } from "~/db/queries/dtsSystemInfo";
 import { getInstanceSystemSettingsByCountryAccountId } from "~/db/queries/instanceSystemSetting";
 import Dialog from "~/components/Dialog";
 import { getCountryAccountsIdFromSession } from "~/util/session";
-import { SelectCountries, InstanceSystemSettings, SelectDtsSystemInfo } from "~/drizzle/schema";
+import { SelectCountries, InstanceSystemSettings, SelectDtsSystemInfo, countryAccountTypes } from "~/drizzle/schema";
 import { getCountryAccountById } from "~/db/queries/countryAccounts";
 import { getCountryById } from "~/db/queries/countries";
 import {
@@ -42,6 +42,7 @@ interface LoaderData {
 	dtsSystemInfo: SelectDtsSystemInfo | null;
 	country: SelectCountries;
 	userRole: string;
+	countryAccountType: string | null;
 }
 
 export const loader: LoaderFunction = authLoaderWithPerm(
@@ -83,7 +84,8 @@ export const loader: LoaderFunction = authLoaderWithPerm(
 			instanceSystemSettings: settings,
 			dtsSystemInfo,
 			country,
-			userRole: userRole
+			userRole: userRole,
+			countryAccountType: countryAccount?.type
 		});
 	}
 );
@@ -269,6 +271,9 @@ export default function Settings() {
 								<strong>Country:</strong> {loaderData.country.name}
 							</li>
 							<li>
+								<strong>Type:</strong> {loaderData.countryAccountType} instance
+							</li>
+							<li>
 								<strong>ISO 3:</strong>{" "}
 								{loaderData.instanceSystemSettings?.dtsInstanceCtryIso3}
 							</li>
@@ -310,11 +315,11 @@ export default function Settings() {
 						</ul>
 					</li>
 					<li>
-						<strong>Website Name:</strong>{" "}
+						<strong>Instance Name:</strong>{" "}
 						{loaderData.instanceSystemSettings?.websiteName}{" "}
 					</li>
 					<li>
-						<strong>Website Logo URL:</strong>{" "}
+						<strong>Instance Logo URL:</strong>{" "}
 						{loaderData.instanceSystemSettings?.websiteLogo}{" "}
 					</li>
 					<li>
