949d32da0dadf73d0b1a79b07dcaba422bc624af
Joel @ Dell3260 <sindicatoesp@gmail.com>
2025-10-30 18:37:27 +0700

    Added sector function.

---
+++ app/drizzle/migrations/20251030113423_sector_functions.sql
@@ -0,0 +1,79 @@
+-- Custom SQL migration file, put your code below! --
+
+
+DROP FUNCTION IF EXISTS "dts_get_sector_all_idonly";
+CREATE FUNCTION "dts_get_sector_all_idonly" (IN "SECTOR_ID" uuid) RETURNS _uuid LANGUAGE plpgsql AS 'BEGIN
+	RETURN ARRAY(
+		WITH RECURSIVE ParentCTE AS (
+			-- Start from the child node
+			SELECT id, sectorname, parent_id
+			FROM sector
+			WHERE id = SECTOR_ID
+
+			UNION ALL
+
+			-- Recursively find parents
+			SELECT s.id, s.sectorname, s.parent_id
+			FROM sector s
+			INNER JOIN ParentCTE p ON s.id = p.parent_id
+		),
+		ChildCTE AS (
+			-- Find all descendants (children)
+			SELECT id, sectorname, parent_id, level
+			FROM sector
+			WHERE id = SECTOR_ID
+			UNION ALL
+			SELECT t.id, t.sectorname, t.parent_id, t.level
+			FROM sector t
+			INNER JOIN ChildCTE c ON t.parent_id = c.id
+		)
+		SELECT *
+		FROM (
+			SELECT id FROM ParentCTE
+			UNION
+			SELECT id FROM ChildCTE
+		) all_records
+	);
+END;
+';
+
+
+DROP FUNCTION IF EXISTS "dts_get_sector_children_idonly";
+CREATE FUNCTION "dts_get_sector_children_idonly" (IN "SECTOR_ID" uuid) RETURNS _uuid LANGUAGE plpgsql AS 'BEGIN
+	RETURN ARRAY(
+		WITH RECURSIVE ChildCTE AS (
+			-- Find all descendants (children)
+			SELECT id
+			FROM sector
+			WHERE id = SECTOR_ID
+			UNION ALL
+			SELECT t.id
+			FROM sector t
+			INNER JOIN ChildCTE c ON t.parent_id = c.id
+		)
+		SELECT id FROM ChildCTE
+	);
+END;
+';
+
+
+DROP FUNCTION IF EXISTS "dts_get_sector_parent_idonly";
+CREATE FUNCTION "dts_get_sector_parent_idonly" (IN "SECTOR_ID" uuid) RETURNS _uuid LANGUAGE plpgsql AS 'BEGIN
+	RETURN ARRAY(
+		WITH RECURSIVE ParentCTE AS (
+		  -- Start from the child node
+		  SELECT id, sectorname, parent_id
+		  FROM sector
+		  WHERE id = SECTOR_ID
+
+		  UNION ALL
+
+		  -- Recursively find parents
+		  SELECT s.id, s.sectorname, s.parent_id
+		  FROM sector s
+		  INNER JOIN ParentCTE p ON s.id = p.parent_id
+		)
+		SELECT id FROM ParentCTE
+	);
+END;
+';
